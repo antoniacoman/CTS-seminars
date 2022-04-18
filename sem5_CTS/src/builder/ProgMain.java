c4491e56b4667f47f39e03198602e01955859840
antoniacoman <91530583+antoniacoman@users.noreply.github.com>
2022-04-19 01:37:13 +0300

    Add files via upload

---
+++ sem5_CTS/src/builder/ProgMain.java
@@ -0,0 +1,10 @@
+package builder;
+
+public class ProgMain {
+
+	public static void main(String[] args) {
+		User u1=new User("anto@gmail.com","anto@gmail.com","anto", "c","bucharest");
+		User u2=new User("anto@gmail.com","anto@gmail.com","anto", "c","0724541495");
+
+	}
+}
