ce369b97f482031000d863e8f548d385793ef16e
antoniacoman <91530583+antoniacoman@users.noreply.github.com>
2022-05-16 14:17:45 +0300

    Add files via upload

---
+++ sem8_CTS/src/decorator/PizzaBasic.java
@@ -0,0 +1,15 @@
+package decorator;
+
+public class PizzaBasic extends APizza{
+
+	@Override
+	public String getIngredient() {
+		return "mozzarela, tomato ";
+	}
+
+	@Override
+	public float getCost() {
+		return 20;
+	}
+	
+}
